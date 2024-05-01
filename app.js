c62f2831ddbdc3c9c1023369a4c1776f23e8b8b7
Pearse Darcy <pearse.darcy@outlook.com>
2024-05-01 20:07:44 +0200

    feat: basic loading page

---
+++ app.js
@@ -0,0 +1,11 @@
+// Create a function which takes the number from the 'loading' element and
+// increments it by 1.
+
+function increment() {
+    let loading = document.querySelector('#loading');
+    loading.textContent = parseInt(loading.textContent) + 1;
+    }
+
+// call increment function when page is loaded
+window.onload = setInterval(increment, 1000);
+
